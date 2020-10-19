0dd3c50130446c5530cd189661855b7e58cb6dc0
IOE <fivetwothreesix@gmail.com>
2020-10-19 20:43:49 +0900

    rename function

--- bimapper.py
+++ bimapper.py
@@ -1,9 +1,9 @@
 
-class AlreadyLinkedException(Exception):
-    """Raised when either x or y has been linked with the other."""
+class AlreadyBoundException(Exception):
+    """Raised when either x or y has been bound with the other."""
 
     def __init__(self, *args):
-        Exception.__init__(self, 'Already Linked', *args)
+        Exception.__init__(self, 'Already Bound', *args)
 
 # Bijection Mapper
 class BiMapper():
@@ -11,20 +11,20 @@ class BiMapper():
         self.x2y = {}
         self.y2x = {}
 
-    def link(self, x, y):
+    def bind(self, x, y):
         if x in self.x2y or y in self.y2x:
             raise AlreadyLinkedException()
         
         self.x2y[x] = y
         self.y2x[y] = x
 
-    def unlink_x(self, x):
+    def unbind_x(self, x):
         if not x in self.x2y: return
 
         y = self.x2y.pop(x)
         self.y2x.pop(y)
 
-    def unlink_y(self, y):
+    def unbind_y(self, y):
         if not y in self.y2x: return
 
         x = self.y2x.pop(y)
