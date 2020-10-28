e5be3db9b4645aebb1cee67d1d97be9fe2ef4d37
IOE <fivetwothreesix@gmail.com>
2020-10-28 19:26:56 +0900

    checkpoint

--- threading/__init__.py
+++ threading/__init__.py
@@ -1,1 +1,15 @@
 from .thread_worker import ThreadWorker
+
+class ExitStatus():
+    def __init__(self):
+        pass
+
+    def result():
+        pass
+
+    def exceptino():
+        pass
+
+def run_once_async(func, args=(), kwargs={}, on_finished=None):
+    worker = ThreadWorker()
+    return worker.run(func, args, kwargs, on_finished)
