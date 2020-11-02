8f98120b3d2a0d1b8c07fee6bc847290e10cca3f
IOE <fivetwothreesix@gmail.com>
2020-11-02 16:49:37 +0900

    del runtime_resource. use the pathlib

--- runtime_resources.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Resources():
-    def __init__(self, basepath):
-        self.basepath = os.path.abspath(basepath)
-    
-    def getpath(self, path):
-        return os.path.join(self.basepath, path)
