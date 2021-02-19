8ce928a6af12de76b00719f73ab95ae730d46cfa
IOE <fivetwothreesix@gmail.com>
2021-02-19 13:16:16 +0900

    merge file_utils into logging

--- file_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import pathlib
-from datetime import datetime
-
-
-def get_unique_log_dir():
-    date = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
-    pid = os.getpid()
-    return pathlib.Path(f'{date}({pid})')
--- logging.py
+++ logging.py
@@ -1,4 +1,13 @@
 import sys
+import os
+import pathlib
+from datetime import datetime
+
+
+def get_unique_log_dir():
+    date = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+    pid = os.getpid()
+    return pathlib.Path(f'{date}({pid})')
 
 class TeeLogger(object):
     def __init__(self, file_path):
