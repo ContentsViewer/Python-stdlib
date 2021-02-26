d5b74b55edbe36a6c67c9dd143e6fffc1abcfd8d
IOE <fivetwothreesix@gmail.com>
2021-02-26 13:37:02 +0900

    sync func support non sleep, return delay time

--- fixedloop.py
+++ fixedloop.py
@@ -7,39 +7,27 @@ class FixedLoop():
 
     """
     
-    interval = 0
+    interval_secs = 0
     _last_fixed_time = 0
-    _last_delay_time = 0
 
-    def __init__(self, interval):
-        self.interval = interval
+    def __init__(self, interval_secs):
+        self.interval_secs = interval_secs
 
     def reset(self):
         self._last_fixed_time = time.time()
 
-    def sync(self):
+    def sync(self, sleep=True):
         if self._last_fixed_time == 0:
             self._last_fixed_time = time.time()
 
         next_time = self.next_fixed_time
         delay_time = next_time - time.time()
         self._last_fixed_time = next_time  # Update the prev time ready for the next call
-        self._last_delay_time = delay_time
 
-        if delay_time > 0:
+        if delay_time > 0 and sleep:
             time.sleep(delay_time)
-            return True
-
-        if delay_time == 0:
-            # return Immediately
-            return True
-
-        if delay_time < 0:
-            return False
-
-    @property
-    def last_delay_time(self):
-        return self._last_delay_time
+        
+        return delay_time
 
     @property
     def last_fixed_time(self):
@@ -47,4 +35,4 @@ class FixedLoop():
 
     @property
     def next_fixed_time(self):
-        return self._last_fixed_time + self.interval
+        return self._last_fixed_time + self.interval_secs
